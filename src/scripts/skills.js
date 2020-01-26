b47200c0169ddb8b820a041466de5fbf6dd1835f
Stanislav Savinykh <savinykhsb@gmail.com>
2020-01-26 20:02:13 +0300

    Секция скиллы как компонент Vue с багом svg

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,43 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+    template: "#skill",
+    props: ['percent', 'name'],
+    methods: {
+        percentCounter(){
+            const circle = this.$refs["color-segment"];
+            const rest = 100 - this.percent;
+            const value = this.percent + " " + rest;
+    
+            circle.style.strokeDasharray = value;
+        }
+    },
+    mounted(){
+        this.percentCounter();
+    }
+}
+
+const skillsRow = {
+    template: "#skills-row",
+    components: {
+        skill
+    },
+    props: ['skill']
+}
+
+new Vue({
+    el: "#skills-component",
+    template: "#skills-list",
+    data(){
+        return {
+            skills: []
+        }
+    },
+    components: {
+        skillsRow
+    },
+    created(){
+        const data = require("../data/skills.json")
+        this.skills = data;
+    }
+})
